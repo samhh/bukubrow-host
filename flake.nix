f47c905f5b7ef5535fb3787942007d7c09e1090f
Sam A. Horvath-Hunt <hello@samhh.com>
2022-11-19 19:32:02 +0000

    Add Nix shell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            buku
+            bukubrow
+            cargo
+            git
+            sqlite
+          ];
+        };
+      });
+}
